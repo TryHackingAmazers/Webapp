e5683754565aab08c14240931d863de6f4e60868
Ansh Agrawal <129865765+AnshAgrawal04@users.noreply.github.com>
2024-06-22 20:29:30 +0530

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -5,4 +5,13 @@ function loadFile(event) {
         output.innerHTML = `<img src="${reader.result}" alt="Uploaded image" style="width: 100%; height: 100%; object-fit: contain;">`;
     };
     reader.readAsDataURL(event.target.files[0]);
-};
+}
+
+// Loading animation
+window.addEventListener('load', function () {
+    const loadingAnimation = document.getElementById('loading-animation');
+    loadingAnimation.style.opacity = '0';
+    setTimeout(() => {
+        loadingAnimation.style.display = 'none';
+    }, 500); // Matches the transition duration
+});
